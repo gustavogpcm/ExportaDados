b48a786af049048dc5af8d83c0d8bcf1ae692c6e
IgorVinoski <igor.vinoski2@gmail.com>
2023-12-28 17:33:04 -0300

    feat all

--- src/config/cron.ts
+++ src/config/cron.ts
@@ -1,8 +1,23 @@
 import { CronTask } from './models/cronTask'
 
 import { makeScheduleExampleUseCase } from '../use-cases/factories/make-schedule-example-use-case'
+import { makeSearchAssistencialUseCase } from '../use-cases/factories/make-search-assistencial'
+import { makeGetAcessTokenUseCase } from '../use-cases/factories/make-get-acess-token-use-case'
+import { formatarData } from '../utils/formatDate_yyyy-mm-dd'
 
-const scheduleExample = makeScheduleExampleUseCase()
-new CronTask(async () => {
-  scheduleExample.execute()
-}).start()
+// const scheduleExample = makeScheduleExampleUseCase()
+// new CronTask(async () => {
+//   scheduleExample.execute()
+// }).start()
+
+const sheduleAssitencialSearch = makeSearchAssistencialUseCase()
+
+export const assistencialSearch = new CronTask(async () => {
+  const getAcessTokenUseCase = makeGetAcessTokenUseCase()
+  await getAcessTokenUseCase.execute()
+  const dataUltimaAlteracao = new Date().toISOString().split('T')[0]
+  console.log('A dataUltimaAlteracao no cron: ', dataUltimaAlteracao)
+  sheduleAssitencialSearch.execute({ dataUltimaAlteracao })
+}, '* 27 17 * * *')
+// '0 23 * * * *
+assistencialSearch.start()
--- src/utils/formatDate_yyyy-mm-dd.ts
+++ src/utils/formatDate_yyyy-mm-dd.ts
@@ -1,12 +1,25 @@
-function padZero(valor: number): string {
-  return valor.toString().padStart(2, '0')
+export function formatarData(data) {
+  if (data instanceof Date) {
+    const dia = String(data.getDate()).padStart(2, '0')
+    const mes = String(data.getMonth() + 1).padStart(2, '0')
+    const ano = data.getFullYear()
+    const hora = String(data.getHours()).padStart(2, '0')
+    const minutos = String(data.getMinutes()).padStart(2, '0')
+    const segundos = String(data.getSeconds()).padStart(2, '0')
+
+    return `${dia}.${mes}.${ano} ${hora}:${minutos}:${segundos}`
+  }
+  return data
 }
 
-/** @description Format date to format yyyy-mm-dd */
-export function formatDate(data: Date): string {
-  const ano = data.getFullYear()
-  const mes = padZero(data.getMonth() + 1)
-  const dia = padZero(data.getDate())
-
-  return `${ano}-${mes}-${dia}`
+export function formatarDatas(objeto) {
+  const objetoFormatado = {}
+  for (const chave in objeto) {
+    if (Object.prototype.hasOwnProperty.call(objeto, chave)) {
+      objetoFormatado[chave] = chave.includes('DATA')
+        ? formatarData(objeto[chave])
+        : objeto[chave]
+    }
+  }
+  return objetoFormatado
 }
