c7539db1702d955c3cc01e290b1e5f666b287647
IgorVinoski <igor.vinoski2@gmail.com>
2024-02-09 13:28:20 -0300

    fix: writeFiles

--- src/utils/writeFile.ts
+++ src/utils/writeFile.ts
@@ -1,12 +1,6 @@
 import fs from 'fs'
 import { makeDirectory } from './makeDirectory'
-import winston from 'winston'
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  transports: [new winston.transports.File({ filename: 'log.json' })],
-})
+import console from 'console'
 
 export async function writeLog(json: any) {
   try {
@@ -16,20 +10,18 @@ export async function writeLog(json: any) {
     const minute = String(now.getMinutes()).padStart(2, '0')
     const date = now.toISOString().slice(0, 10).replace(/-/g, '')
     const fileName = `${date}-${hour}-${minute}.json`
-    const filePath = `${folder}${fileName}`
+    const filePath = `${folder}/${fileName}`
 
     try {
       await fs.promises.access(filePath)
-      await fs.promises.appendFile(filePath, json)
+      await fs.promises.appendFile(filePath, JSON.stringify(json))
       console.log(`Dados foram anexados ao arquivo ${fileName} em ${folder}`)
-      logger.info(`Dados foram anexados ao arquivo ${fileName} em ${folder}`)
     } catch (err) {
-      await fs.promises.writeFile(filePath, json)
-      console.error(`Arquivo ${fileName} foi criado em ${folder}`)
-      logger.error(`Arquivo ${fileName} foi criado em ${folder}`)
+      await fs.promises.writeFile(filePath, JSON.stringify(json))
+      console.error(`ERRO: Arquivo ${fileName} foi criado em ${folder}`)
     }
   } catch (err) {
+    console.error('ERRO: ' + err)
     console.log(`Erro ao criar o diretório ou o arquivo: ${err.message}`)
-    logger.error(`Erro ao criar o diretório ou o arquivo: ${err.message}`)
   }
 }
